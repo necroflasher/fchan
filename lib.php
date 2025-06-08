dacc3555236ecc417f74ba50b0367e32e1fac1b6
necroflasher <210901387+necroflasher@users.noreply.github.com>
2025-06-08 16:11:46 +0000

    tweak filesize format
    - rough kb precision
    - nbsp to avoid wrap for layout reasons

--- lib.php
+++ lib.php
@@ -45,7 +45,7 @@ function format_fs($bytes)
 	if ($bytes < 1024)
 		return $bytes.' B';
 	else if ($bytes < 1024*1024)
-		return number_format($bytes/1024, 2).' KB';
+		return number_format($bytes/1024, 0).'&nbsp;KB';
 	else
-		return number_format($bytes/(1024*1024), 2).' MB';
+		return number_format($bytes/(1024*1024), 1).'&nbsp;MB';
 }
